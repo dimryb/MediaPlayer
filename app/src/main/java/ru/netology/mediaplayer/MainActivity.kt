b24267b9718e9fdff32e5d1c5527116a80eb54f6
dmitry <dimryb@bk.ru>
2022-12-14 19:57:20 +0700

    Простое воспроизведение файла

--- app/src/main/java/ru/netology/mediaplayer/MainActivity.kt
+++ app/src/main/java/ru/netology/mediaplayer/MainActivity.kt
@@ -1,11 +1,17 @@
 package ru.netology.mediaplayer
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<View>(R.id.play).setOnClickListener {
+            MediaPlayer.create(this, R.raw.ring).start()
+        }
     }
 }
